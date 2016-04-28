6ae5766c995834646a94704aaf4a1dabecd5e667
Khurram Ali <khurram.ali@riksof.com>
2016-04-28 13:03:40 +0500

    Add -1 as a non-magic number

--- reviewers/ESLINTChecker.js
+++ reviewers/ESLINTChecker.js
@@ -91,7 +91,7 @@ checker.prototype = {
       'no-eval': this.SEVERITY_MEDIUM,
       'no-lone-blocks': this.SEVERITY_HIGH,
       'no-loop-func': this.SEVERITY_HIGH,
-      'no-magic-numbers': [this.SEVERITY_HIGH, {'ignore': [0, 1]}],
+      'no-magic-numbers': [this.SEVERITY_HIGH, {'ignore': [0, 1, -1]}],
       'no-multi-spaces': this.SEVERITY_HIGH,
       'no-native-reassign': this.SEVERITY_HIGH,
       'no-new': this.SEVERITY_HIGH,
